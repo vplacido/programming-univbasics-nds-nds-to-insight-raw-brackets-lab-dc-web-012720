733e0138e93f73913ba8ea9a5068b41faa08dc44
Valentin Placido <valentinplacido25@gmail.com>
2020-01-18 05:07:43 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,17 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  newHash = {}
+  directorIndex = 0 
+  while directorIndex < nds.length do 
+    name = nds[directorIndex][:name]
+    movieIndex = 0 
+    newHash[name] = 0 
+    while movieIndex < nds[directorIndex][:movies].length do 
+      newHash[name] += nds[directorIndex][:movies][movieIndex][:worldwide_gross] 
+      movieIndex += 1 
+    end 
+    directorIndex+=1 
+  end 
+  newHash
 end
