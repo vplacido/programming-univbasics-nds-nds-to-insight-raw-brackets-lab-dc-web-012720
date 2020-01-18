9c075ffa4f1c1761d8b2171a7f4b461aeffa6599
Valentin Placido <valentinplacido25@gmail.com>
2020-01-18 05:07:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,9 +3,16 @@ require 'directors_database'
 
 def directors_totals(nds)
   newHash = {}
-  director = 0 
-  while i < nds.length do 
-    result[nds[i][:name]] = 
-    movie = 0 
-  
+  directorIndex = 0 
+  while directorIndex < nds.length do 
+    name = nds[directorIndex][:name]
+    movieIndex = 0 
+    newHash[name] = 0 
+    while movieIndex < nds[directorIndex][:movies].length do 
+      newHash[name] += nds[directorIndex][:movies][movieIndex][:worldwide_gross] 
+      movieIndex += 1 
+    end 
+    directorIndex+=1 
+  end 
+  newHash
 end
